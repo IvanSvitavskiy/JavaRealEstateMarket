07173f8bb35846e85cf7e589b01439ef451a9fcf
Azazelloooo <azat.260604@gmail.com>
2023-05-18 00:36:01 +0300

    created Building model

---
+++ RealEstateMarket/src/main/java/com/example/RealEstateMarket/Models/Building.java
@@ -0,0 +1,23 @@
+package com.example.RealEstateMarket.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Building {
+    @Getter
+    @Setter
+    private String title;
+    @Getter
+    @Setter
+    private String desc;
+    @Getter
+    @Setter
+    private int price;
+    @Getter
+    @Setter
+    private String city;
+    @Getter
+    @Setter
+    private String address;
+
+}
