c8582f0595b335ae77360fe0f6700eeb28bf39f1
Azazelloooo <azat.260604@gmail.com>
2023-05-18 18:04:27 +0300

    reconstructed building model for db

--- RealEstateMarket/src/main/java/com/example/RealEstateMarket/Models/Building.java
+++ RealEstateMarket/src/main/java/com/example/RealEstateMarket/Models/Building.java
@@ -1,31 +1,51 @@
 package com.example.RealEstateMarket.Models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "buildings")
 public class Building {
     @Getter
     @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+
     @Getter
     @Setter
+    @Column(name = "id")
     private String title;
+
     @Getter
     @Setter
+    @Column(name = "desc", columnDefinition = "text")
     private String desc;
+
     @Getter
     @Setter
+    @Column(name = "price")
     private int price;
+
     @Getter
     @Setter
+    @Column(name = "city")
     private String city;
+
     @Getter
     @Setter
+    @Column(name = "address")
     private String address;
+
     @Getter
     @Setter
+    @Column(name = "owner")
     private String owner;
 
 }
--- RealEstateMarket/src/main/java/com/example/RealEstateMarket/Services/BuildingService.java
+++ RealEstateMarket/src/main/java/com/example/RealEstateMarket/Services/BuildingService.java
@@ -9,9 +9,6 @@ import java.util.List;
 @Service
 public class BuildingService {
     private List<Building> buildings = new ArrayList<>();
-    private long cur_id = 0L;
-
-    {buildings.add(new Building(++cur_id, "first", "dadsa",4454, "tambov",  "oktgl", "ivan"));}
 
     public List<Building> getAllBuildings(){
         return buildings;
