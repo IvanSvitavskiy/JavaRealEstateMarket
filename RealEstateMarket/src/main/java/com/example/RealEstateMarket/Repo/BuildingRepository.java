3c8a2222dd43bb40cdc4a41073d2cfdfbe7c1fe5
IvanSvitavskiy <ivan.svitavskiy@mail.ru>
2023-05-18 18:06:56 +0300

    created BuildingRepository

---
+++ RealEstateMarket/src/main/java/com/example/RealEstateMarket/Repo/BuildingRepository.java
@@ -0,0 +1,7 @@
+package com.example.RealEstateMarket.Repo;
+
+import com.example.RealEstateMarket.Models.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+}
